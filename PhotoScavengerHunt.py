182672d8e685e7d750b4b3976904734400ce70eb
bt-png <110639339+bt-png@users.noreply.github.com>
2025-06-18 15:57:44 -0700

    Update PhotoScavengerHunt.py

--- PhotoScavengerHunt.py
+++ PhotoScavengerHunt.py
@@ -4,3 +4,12 @@ st.header('Photo Scavenger Hunt')
 
 with st.container():
   st.write('Find a Pull-Off')
+
+conn = st.connection("neon", type="sql")
+
+# Perform query.
+df = conn.query('SELECT * FROM home;', ttl="10m")
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
